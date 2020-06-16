3d562b42fa2640ca8674a24c0f0b85c85b687bbd
DoubleHelix <tjrnyc5@gmail.com>
2020-06-15 23:27:33 -0400

    added website coloumn for artist data table

--- starter_code/env/app.py
+++ starter_code/env/app.py
@@ -64,6 +64,7 @@ class Artist(db.Model):
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
+    website_link = db.Column(db.String(120))
     facebook_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean, default = False)
     seeking_description = db.Column(db.String(250))
